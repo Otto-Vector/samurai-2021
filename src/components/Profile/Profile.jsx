d39f525d6ef9bb8cd21bd5213ece49ee92fb9682
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2021-09-10 23:27:44 +0300

    dialogs css changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import s from './Profile.module.css';
+import styles from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = (props) => {
 
     return (
-        <div>
+        <div className={styles.wrapper}>
             <ProfileInfo/>
             <MyPosts posts={props.posts}/>
         </div>
