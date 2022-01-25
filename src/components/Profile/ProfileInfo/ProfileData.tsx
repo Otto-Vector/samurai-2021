4cf0204bc295aab98a7f0c978dab4025c6bec0d6
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2022-01-25 15:56:47 +0300

    some codestyle fixes

--- src/components/Profile/ProfileInfo/ProfileData.tsx
+++ src/components/Profile/ProfileInfo/ProfileData.tsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import styles from './ProfileInfo.module.css';
-import Socials from "../../common/Socials/Socials";
-import ProfileStatusContainer from "./ProfileStatus/ProfileStatusContainer";
-import {ProfileType} from "../../../redux/types/types";
+import React from 'react'
+import styles from './ProfileInfo.module.css'
+import Socials from '../../common/Socials/Socials'
+import ProfileStatusContainer from './ProfileStatus/ProfileStatusContainer'
+import { ProfileType } from '../../../redux/types/types'
 
 type OwnPropsType = {
-  profile: ProfileType | null
-  isAuthProfile: boolean
-  activateEditMode: ()=>void
+    profile: ProfileType | null
+    isAuthProfile: boolean
+    activateEditMode: () => void
 }
 
 const ProfileData: React.FC<OwnPropsType> = (
-  {isAuthProfile,activateEditMode,profile}) => {
+    { isAuthProfile, activateEditMode, profile } ) => {
 
-  return <div>
-    <h2 className={styles.fullName}>{profile?.fullName}</h2>
-    <ProfileStatusContainer/>
-    <p className={styles.aboutMe}>{profile?.aboutMe}</p>
-    <div className={styles.socials}>
-      <Socials contacts={profile?.contacts}/>
-    </div>
-    <div className={styles.jobAlert}>
-      <p><strong>Поиск работы:</strong></p>
-      {!profile?.lookingForAJob ? <p>НЕТ, не ищу</p> :
-        <div>
-          <p>ДА, ищу</p>
-          <strong><p>Skills:</p></strong>
-          <p>{profile?.lookingForAJobDescription}</p>
+    return <div>
+        <h2 className={ styles.fullName }>{ profile?.fullName }</h2>
+        <ProfileStatusContainer/>
+        <p className={ styles.aboutMe }>{ profile?.aboutMe }</p>
+        <div className={ styles.socials }>
+            <Socials contacts={ profile?.contacts }/>
         </div>
-      }
+        <div className={ styles.jobAlert }>
+            <p><strong>Поиск работы:</strong></p>
+            { !profile?.lookingForAJob ? <p>НЕТ, не ищу</p> :
+                <div>
+                    <p>ДА, ищу</p>
+                    <strong><p>Skills:</p></strong>
+                    <p>{ profile?.lookingForAJobDescription }</p>
+                </div>
+            }
+        </div>
+        <div className={ styles.editButtonWrapper }>
+            { isAuthProfile && <button className={ styles.editButton } onClick={ activateEditMode }>Edit</button> }
+        </div>
     </div>
-    <div className={styles.editButtonWrapper}>
-      {isAuthProfile && <button className={styles.editButton} onClick={activateEditMode}>Edit</button>}
-    </div>
-  </div>
 }
 
-const memoizedProfileData = React.memo(ProfileData)
+const memoizedProfileData = React.memo( ProfileData )
 
-export default memoizedProfileData;
+export default memoizedProfileData
--- src/components/common/Socials/Socials.tsx
+++ src/components/common/Socials/Socials.tsx
@@ -1,38 +1,37 @@
-import React from "react";
+import React from 'react'
 import styles from './Socials.module.css'
-import {socialsImageSource, SocialsImageSourceType} from "./SocialsImageSource";
-import {ContactsType} from "../../../redux/types/types";
+import { socialsImageSource, SocialsImageSourceType } from './SocialsImageSource'
+import { ContactsType } from '../../../redux/types/types'
 
 type OwnProps = {
-  contacts: ContactsType | undefined
+    contacts: ContactsType | undefined
 }
 
+const Socials: React.FC<OwnProps> = ( { contacts } ) => {
+    // защита от undefined входящего элемента
+    if (contacts === undefined) return null
 
-const Socials = ({contacts}: OwnProps) => {
-  // защита от undefined входящего элемента
-  if (contacts === undefined) return null
 
-  type HasOwnKeysType = Extract<keyof ContactsType, keyof SocialsImageSourceType>
-  // фильтруем contacts в массив ключей, присутствующих в socialsImageSource
-  const filteredBySource = Object.keys(contacts)
-    .filter((key) => socialsImageSource.hasOwnProperty(key))
-  //делаем то же самое с ключами types
+    // фильтруем contacts в массив ключей, присутствующих в socialsImageSource
+    const filteredBySource = Object.keys( contacts )
+        .filter( ( key ) => socialsImageSource.hasOwnProperty( key ) )
+    //делаем то же самое с ключами types
+    type HasOwnKeysType = Extract<keyof ContactsType, keyof SocialsImageSourceType>
 
-  return (
-    <div className={ styles.socials }>
-      { filteredBySource.map((key) => {
-        // let image = socialsImageSource[key as keyof SocialsImageSourceType],
-        let image = socialsImageSource[key as HasOwnKeysType],
-        hrefIs = contacts[key as HasOwnKeysType] || undefined,
-        imageUrl = image ? image[hrefIs ? 'filled' : 'empty'] : undefined
+    return (
+        <div className={ styles.socials }>
+            { filteredBySource.map( ( key ) => {
+                let image = socialsImageSource[key as HasOwnKeysType],
+                    hrefIs = contacts[key as HasOwnKeysType] || undefined,
+                    imageUrl = image ? image[hrefIs ? 'filled' : 'empty'] : undefined
 
-        return <a key={ key } target="_blank" rel="noopener noreferrer" href={ hrefIs }>
-          <img alt={ key } title={ key } src={ imageUrl }/>
-        </a>
-      })
-      }
-    </div>
-  )
+                return <a key={ key } target="_blank" rel="noopener noreferrer" href={ hrefIs }>
+                    <img alt={ key } title={ key } src={ imageUrl }/>
+                </a>
+            } )
+            }
+        </div>
+    )
 }
 
 export default Socials
